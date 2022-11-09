46a9b8ad4b7bd7f78efac3b7897fb9adebe27e73
Arqestra <mshawdietrich@gmail.com>
2022-11-09 10:50:42 +0000

    Changes

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+const routes = require('./route')
+server.use('/route', routes)
+
+server.get('/', (req, res) => res.send('Welcome to the Anonymous'))
+
+module.exports = server
