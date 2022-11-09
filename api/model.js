e6e5fad58c180b1f4c31d203c294da2ecfa53229
Arqestra <mshawdietrich@gmail.com>
2022-11-09 11:37:10 +0000

    API Changes

--- api/model.js
+++ api/model.js
@@ -6,4 +6,18 @@ module.exports = class Post {
         this.name = data.name;
         this.content = data.content;
     };
+
+    static get all() {
+        return new Promise (async (resolve, reject) => {
+            try {
+                const result = await db.query('SELECT * FROM posts;')
+                const allPosts = result.rows.map(a => ({id: a.id, title: a.title, content: a.content }))
+                resolve(allPosts);
+            } catch (err) {
+                reject("Error retrieving posts")
+            }
+        })
+    }
+
+    
 }
