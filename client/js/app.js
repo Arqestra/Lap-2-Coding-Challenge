d001009a45c85e44a463e32e44c52e01a59446ea
Arqestra <mshawdietrich@gmail.com>
2022-11-10 09:20:19 +0000

    CSS Change

---
+++ client/js/app.js
@@ -0,0 +1,20 @@
+async function post(e){
+    e.preventDefault();
+    try {
+        const options = {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
+        }
+        
+        const response = await fetch('http://localhost:3000/posts/', options);
+        const { id, err } = await response.json();
+        if(err) { 
+            throw Error(err) 
+        } else {
+            window.location.hash = `#/${id}`
+        }
+    } catch (err) {
+        console.warn(err);
+    }
+}
